3498d31c43a8da1f4df303e2c01bffe63a612568
Awnon Bhowmik <awnonbhowmik@users.noreply.github.com>
2019-04-24 02:48:54 -0400

    First Commit

---
+++ Heighway_Dragon_Fractal_Curve.py
@@ -0,0 +1,26 @@
+import turtle
+turtle.speed(10000)
+turtle.hideturtle()
+wn=turtle.Screen()
+wn.bgcolor("black")
+turtle.pencolor("green")
+ 
+def dragoncurve(l,n):
+  for i in range(1):
+    def x(n):
+            if n==0:
+                    return
+            x(n-1)
+            turtle.right(90)
+            y(n-1)
+            turtle.forward(l)
+    def y(n):
+            if n==0:
+                    return
+            turtle.forward(l)
+            x(n-1)
+            turtle.left(90)
+            y(n-1)
+    turtle.fd(l)
+    x(n)
+dragoncurve(5,15)
